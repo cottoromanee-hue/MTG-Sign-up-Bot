bb824d88a3402e3c80194cc4c5150ea26a9c854b
cottoromanee-hue <cotto.romanee@gmail.com>
2026-07-27 16:42:14 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Gathering Magic", // required by Discord API, not shown in the client
         state: "Gathering Maigc",     // this is what people actually see
         type: 3,               // Custom
       },
